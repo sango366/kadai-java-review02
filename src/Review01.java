af677a131c8ed77f4328ae6e78d601165a21ae91
sango366 <txobitxobi.1@gmail.com>
2024-03-24 22:59:03 +0900

    税込価格を計算しよう作成ファイルのコミット：Review01.java

---
+++ src/Review01.java
@@ -0,0 +1,18 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int money = 100;
+        double taxRate = 10.0;
+        int tax_amount =  tax(money,10);
+        int moneyIncludingTax = tax_amount + money;
+        System.out.println(money + "円の商品の税込価格は" + moneyIncludingTax + "円（消費税は" + tax_amount  + "円）です。");
+
+    }
+
+    public static int tax(int money, double taxRate) {
+        double taxAmountDouble = money * (taxRate / 100);
+        int taxAmount = (int) taxAmountDouble;
+        return taxAmount;
+    }
+}
