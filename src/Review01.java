d27341428eedb8c0e65969e5907876937b898170
sango366 <txobitxobi.1@gmail.com>
2024-03-24 23:03:05 +0900

    int tax_amount =  tax(money,taxRate);

--- src/Review01.java
+++ src/Review01.java
@@ -4,7 +4,7 @@ public class Review01 {
     public static void main(String[] args) {
         int money = 100;
         double taxRate = 10.0;
-        int tax_amount =  tax(money,10);
+        int tax_amount =  tax(money,taxRate);
         int moneyIncludingTax = tax_amount + money;
         System.out.println(money + "円の商品の税込価格は" + moneyIncludingTax + "円（消費税は" + tax_amount  + "円）です。");
 
